55b7477a3b5b30bce48ebaa1bea7c12b99b746a2
lakshan-cd <87594213+lakshan-cd@users.noreply.github.com>
2025-07-28 14:37:59 +0530

    Add PredictNewCompany component to financials page for enhanced company prediction features.

--- app/financials/page.tsx
+++ app/financials/page.tsx
@@ -1,3 +1,4 @@
+import { PredictNewCompany } from "./components/predict-new-company";
 import ViewAllCompanies from "./components/view-all-companies";
 
 const Page = () => {
@@ -5,6 +6,10 @@ const Page = () => {
         <>
                 <div className="px-4 sm:px-4 md:px-8 lg:px-16 my-12">
                     <ViewAllCompanies page="financials" />
+
+                    <div className="mt-12">
+                        <PredictNewCompany />
+                    </div>
                 </div>
         </>
     )
